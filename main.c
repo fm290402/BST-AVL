06988f845827dfe20f81cfb7f80197f3d889f5dc
FadhilM <76807813+fm290402@users.noreply.github.com>
2021-07-08 13:44:16 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "datapegawai.h"
+#include "datapegawai.c"
+
+void addPegawai() {
+	infotype X;
+	
+	printf ("Nama Pegawai :\n ");
+	scanf ("%[^\n]", X.nama);
+	fflush(stdin);
+	printf ("ID Pegawai : \n");
+	scanf ("%d", &X.id);
+	fflush(stdin);
+	
+	TambahPegawai(&root,X);
+}
+
+void removePegawai() {
+	infotype X;
+	
+	printf ("ID Pegawai : \n");
+	scanf ("%d", &X.id)
+		
+	HapusPegawai(&root, address node);
+}
+
+void searchPegawai() {
+	
+	infotype X;
+	
+	printf ("ID Pegawai yang ingin dihapus : \n");
+	scanf ("%d", &X.id)
+		
+	CariPegawaiDenganID(P, id);
+}
+
+
+
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+	int ch;
+	Tree T;
+	infotype data;
+
+	CreateEmpty (&T, data);
+	
+	while (1) {
+		scanf ("%c", &ch);
+		switch (ch) {		
+			case 1: // Print tree
+				PrintBagan(T);
+			case 2: //Tambah Pegawai
+				addPegawai();
+			case 3: //Delete Pegawai
+				removePegawai();
+			case 4: //Cari pegawai
+				searchPegawai();
+			case 0: //Exit Aplikasi	
+				return 0;
+		}	
+	}
 }
