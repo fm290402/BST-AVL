87309c203c412a528bf6d28e1edc79a2ad180cb8
FadhilM <76807813+fm290402@users.noreply.github.com>
2021-07-08 13:52:49 +0700

    Update main.c

--- main.c
+++ main.c
@@ -4,40 +4,6 @@
 #include "datapegawai.h"
 #include "datapegawai.c"
 
-void addPegawai() {
-	infotype X;
-	
-	printf ("Nama Pegawai :\n ");
-	scanf ("%[^\n]", X.nama);
-	fflush(stdin);
-	printf ("ID Pegawai : \n");
-	scanf ("%d", &X.id);
-	fflush(stdin);
-	
-	TambahPegawai(&root,X);
-}
-
-void removePegawai() {
-	infotype X;
-	
-	printf ("ID Pegawai : \n");
-	scanf ("%d", &X.id)
-		
-	HapusPegawai(&root, address node);
-}
-
-void searchPegawai() {
-	
-	infotype X;
-	
-	printf ("ID Pegawai yang ingin dihapus : \n");
-	scanf ("%d", &X.id)
-		
-	CariPegawaiDenganID(P, id);
-}
-
-
-
 int main()
 {
 	int ch;
@@ -62,3 +28,36 @@ int main()
 		}	
 	}
 }
+
+void tambahPegawai() {
+	infotype a;
+	
+	printf ("Nama Pegawai :\n ");
+	scanf ("%[^\n]", a.nama);
+	fflush(stdin);
+	printf ("ID Pegawai : \n");
+	scanf ("%d", &a.id);
+	fflush(stdin);
+	
+	TambahPegawai(&root,a);
+}
+
+void hapusPegawai() {
+	infotype a;
+	
+	printf ("ID Pegawai : \n");
+	scanf ("%d", &a.id)
+		
+	HapusPegawai(&root, address node);
+}
+
+void cariPegawai() {
+	
+	infotype a;
+	
+	printf ("ID Pegawai yang ingin dihapus : \n");
+	scanf ("%d", &a.id)
+		
+	CariPegawaiID(P, id);
+}
+
