d3ba62d86b4779255f6d2209fe2fe6417002cbe9
FadhilM <76807813+fm290402@users.noreply.github.com>
2021-07-08 13:03:32 +0700

    Method inorder

--- datapegawai.c
+++ datapegawai.c
@@ -1,2 +1,14 @@
 #include "datapegawai.h"
 #include <stdio.h>
+
+/*  Traversal InOrder
+    Author : Fadhil Muhammad
+    I.S. : T terdefinisi
+    F.S. : semua simpul T sudah diproses secara InOrder : kiri, akar, kanan */
+void InOrder(BSTree T){
+	if (T != Nil){
+		InOrder(root->left);
+		printf("%d ", root->id);
+		InOrder(root->right);
+	}
+}
